333c50a9734cfa29677000e317236d0b1e76808d
UnimportantWank <Xeromancer@gmail.com>
2017-11-10 16:56:46 -0600

    actually add the js

---
+++ js/scroll.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        $(".top").css("opacity", 1.1 - $(window).scrollTop() / 250);
+    });
+    $(window).scroll(function() {
+        $("#about").css("opacity",3.3 - $(window).scrollTop() / 350);
+    });
+});
