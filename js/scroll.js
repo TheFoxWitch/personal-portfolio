c1d8c3f5b4cd4eaa90c91745aff369963ae840ab
UnimportantWank <Xeromancer@gmail.com>
2017-11-10 19:46:59 -0600

    tweak

--- js/scroll.js
+++ js/scroll.js
@@ -4,6 +4,6 @@ $(document).ready(function() {
         $(".top").css("opacity", 1.0 - $(window).scrollTop() / 250);
     });
     $(window).scroll(function() {
-        $(".description").css("opacity",2.7 - $(window).scrollTop() / 350);
+        $(".description").css("opacity",2.6 - $(window).scrollTop() / 350);
     });
 });
