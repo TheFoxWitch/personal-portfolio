401a0de2b317fec517371ab49b89d526a46c52d0
UnimportantWank <Xeromancer@gmail.com>
2017-11-19 13:55:52 -0600

    change scroll opacity for darker font

--- js/scroll.js
+++ js/scroll.js
@@ -4,6 +4,6 @@ $(document).ready(function() {
         $(".top").css("opacity", 1.0 - $(window).scrollTop() / 250);
     });
     $(window).scroll(function() {
-        $(".description").css("opacity",2.9 - $(window).scrollTop() / 390);
+        $(".description").css("opacity",2.6 - $(window).scrollTop() / 390);
     });
 });
